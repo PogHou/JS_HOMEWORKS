5db19a8063c444e08b700f3f76da1d2edeb0e17f
Aleksey S <dubpseudonym@gmail.com>
2025-12-05 13:51:10 +0200

    Completed hw13

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) sum += arr[i];
+console.log("Sum=", sum);
+
+let sumSquares = 0;
+for (let i = 0; i < arr.length; i++) sumSquares += Math.pow(arr[i], 2);
+console.log("Sum of squares=", sumSquares);
